4b514b9195ffbaa6c836becb85b3d97c5c60e6d8
januarcexp <mccoystar1818@gmail.com>
2026-04-20 09:35:39 +0700

    cipta menambahkan file test bukalapak

---
+++ tests/bukalapak.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('bukalapak test', async ({ page }) => {
+  await page.goto('https://www.bukalapak.com/');
+  await page.waitForLoadState('domcontentloaded');
+  await page.getByRole('link', { name: 'Paket Data' }).click();
+  await page.getByRole('button', { name: 'Lanjut' }).click();
+  await expect(page.getByText('Form harus diisi dulu ya')).toBeVisible();
+});
